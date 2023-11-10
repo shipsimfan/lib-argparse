879249290d8dbc3038b0d6c92ea7e8a7946dd311
Lance Hart <shipsimfan@hotmail.com>
2023-11-10 14:29:37 -0500

    Trying to make it const

--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,10 @@
 //! # lib-argparse
+//! Library for parsing command line arguments
 //!
-//! Library for parsing arguments
+//! ## Concepts
+//! **TODO**
 //!
-//! ## Concept
-//!
-//! Argument parsing is performed in a tree. Each node of the tree is a [`Parser`].
-//! [`Parser`]s can have any number of [`FlagArgument`]s and one [`TerminalArgument`].
-//!
-//! ### Flag Arguments
-//!
-//! [`FlagArgument`]s are arguments that start with a short prefix (default: `"-"`) or a long prefix (default: `"--"`)
-//! A [`FlagArgument`] can be one of the following:
-//!  - `HelpFlag`
-//!  - [`ActionFlag`]
-//!  - [`ValueFlag`]
-//!
-//! #### Help Flag
-//! A `HelpFlag` generates and displays a help for the current parser and exits the program when matched.
-//!
-//! #### Action Flag
-//! An [`ActionFlag`] runs a function on matching.
-//!
-//! The function can take one of the following as parameters:
-//!  - `()`
-//!  - `(&str)`
-//!  - `(String)`
-//!  - `(Vec<String>)`
-//!
-//! The function can return one of the following:
-//!  - `()`
-//!  - `Result<(), E>` where `E` is any type
-//!
-//! #### Value Flag
-//! A [`ValueFlag`] will construct a value using a [`ValueParser`] and pass it to a function.
-//! The [`ValueParser`] is given access to the argument stream so it can be variable length.
-//! The function can return the same types as an action flag.
-//!
-//! ### Terminal Arguments
-//! [`TerminalArgument`]s are how the tree of [`Parser`]s is formed.
-//! A [`TerminalArgument`] can be one of the following:
-//!  - `None`
-//!  - [`Command`]
-//!  - [`Positionals`]
-//!
-//! #### Commands
-//! [`Command`]s form the branches of the tree.
-//! [`Command`]s are made of an unordered list of strings representing commands.
-//! Each command leads to a [`Parser`] which continues parsing the remaining arguments.
-//!
-//! #### Positionals
-//! [`Positionals`] and `None` form the leaves of the tree.
-//! [`Positionals`] are made from an ordered list of [`PositionalParser`]s.
+//! ## Usage
+//! **TODO**
 
-#![feature(associated_type_bounds)]
-
-mod error;
-mod flag;
-mod parser;
-mod stream;
-mod terminal;
-
-#[doc(hidden)]
-pub mod macros;
-
-pub use error::Error;
-pub use flag::{
-    ActionFlag, FlagArgument, FlagKind, SimpleValueParser, SimpleValueParserOS,
-    SimpleValueParserTry, ValueFlag, ValueParser,
-};
-pub use parser::Parser;
-pub use stream::ArgStream;
-pub use terminal::{
-    CollectOsPositionalParser, CollectPositionalParser, Command, PositionalParser, Positionals,
-    SimplePositionalParser, SimplePositionalParserOS, SimplePositionalParserTry, TerminalArgument,
-};
-
-pub(crate) use flag::FlagSet;
+#![deny(missing_docs)]
--- src/macros/terminal/mod.rs
+++
@@ -1,5 +0,0 @@
-mod command;
-mod positionals;
-
-#[doc(hidden)]
-pub use command::__command;
