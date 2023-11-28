123ff5757aa4dcf3ccb0c8fb8be550db1d3ce4df
Lance Hart <shipsimfan@hotmail.com>
2023-11-28 12:32:53 -0500

    Removed no exit from help flag macro

--- macros/src/help_flag.rs
+++ macros/src/help_flag.rs
@@ -3,24 +3,13 @@ use proc_macro_util::{to_tokens, Generator, Parse, Parser, Result, ToTokens};
 
 pub struct HelpFlag {
     flag_name: FlagName,
-    exit: bool,
 }
 
 impl<'a> Parse<'a> for HelpFlag {
     fn parse(parser: &mut Parser<'a>) -> Result<Self> {
         let flag_name = parser.parse()?;
 
-        let exit = if let Some(identifier) = parser.identifier() {
-            if identifier != "noexit" {
-                return Err(parser.error("expected `noexit` or the end"));
-            }
-
-            false
-        } else {
-            true
-        };
-
-        Ok(HelpFlag { flag_name, exit })
+        Ok(HelpFlag { flag_name })
     }
 }
 
@@ -31,11 +20,5 @@ impl ToTokens for HelpFlag {
         to_tokens! { generator
             ::argparse::HelpFlagArgument::new()#flag_name
         };
-
-        if !self.exit {
-            to_tokens! { generator
-                .set_no_exit()
-            }
-        }
     }
 }
