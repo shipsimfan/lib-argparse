8b0dbcf72652c529558b353817ab9cf60a2f826a
Lance Hart <shipsimfan@hotmail.com>
2023-12-29 11:32:18 -0500

    Implemented commands macro

--- macros/src/commands.rs
+++ macros/src/commands.rs
@@ -1,13 +1,33 @@
-use proc_macro_util::{Generator, Parse, Parser, Result, ToTokens};
+use proc_macro_util::{
+    ast::{Expression, Punctuated},
+    to_tokens, Generator, Parse, Parser, Result, ToTokens, Token,
+};
 
-pub struct Commands {}
+pub struct Commands<'a> {
+    commands: Punctuated<Expression<'a>, Token![,]>,
+}
 
-impl<'a> Parse<'a> for Commands {
+impl<'a> Parse<'a> for Commands<'a> {
     fn parse(parser: &mut Parser<'a>) -> Result<Self> {
-        Ok(Commands {})
+        let mut commands = Punctuated::new();
+        while !parser.empty() {
+            commands.push_element(parser.parse()?);
+
+            if parser.peek::<Token![,]>() {
+                commands.push_seperator(parser.parse()?);
+            }
+        }
+
+        Ok(Commands { commands })
     }
 }
 
-impl ToTokens for Commands {
-    fn to_tokens(&self, generator: &mut Generator) {}
+impl<'a> ToTokens for Commands<'a> {
+    fn to_tokens(&self, generator: &mut Generator) {
+        let Commands { commands } = self;
+
+        to_tokens! { generator
+            ::argparse::Commands::new(&[#commands])
+        }
+    }
 }
