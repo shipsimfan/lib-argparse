09238381cd4959b29d95170228021d15a56d0c8a
Lance Hart <me@lancehart.ca>
2024-11-09 17:18:48 -0500

    Started developing version 4

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,7 @@
-//! # lib-argparse
 //! Library for parsing command line arguments
-//!
-//! ## Concepts
-//! **TODO**
-//!
-//! ## Usage
-//! **TODO**
 
 #![deny(missing_docs)]
-#![feature(const_trait_impl)]
-#![feature(const_mut_refs)]
-
-mod error;
-mod flag_argument;
-mod parser;
-mod terminal_argument;
-
-pub use error::{Error, Result};
-pub use flag_argument::{
-    ConfigFlagArgument, FlagArgument, FlagClass, HelpFlagArgument, ParsingFlagArgument,
-    SimpleFlagArgument, VersionFlagArgument,
-};
-pub use macros::{
-    command, commands, config_flag, help_flag, parser, parsing_flag, parsing_positional,
-    positionals, simple_flag, simple_positional, version_flag,
-};
-pub use parser::Parser;
-pub use terminal_argument::{
-    Command, Commands, ParsingPositionalArgument, PositionalArgument, PositionalTerminalArgument,
-    SimplePositionalArgument, TerminalArgument,
-};
+#![deny(rustdoc::private_intra_doc_links)]
+#![deny(rustdoc::unescaped_backticks)]
+#![deny(rustdoc::redundant_explicit_links)]
+#![warn(rustdoc::broken_intra_doc_links)]
--- src/terminal_argument/positional/mod.rs
+++
@@ -1,9 +0,0 @@
-mod parsing;
-mod simple;
-mod r#struct;
-mod r#trait;
-
-pub use parsing::ParsingPositionalArgument;
-pub use r#struct::PositionalTerminalArgument;
-pub use r#trait::PositionalArgument;
-pub use simple::SimplePositionalArgument;
