6afd0529a53116d32abadfef3ce12953ce0b076a
Lance Hart <me@lancehart.ca>
2024-12-11 19:02:16 -0500

    Updated dependencies and fixed formatting

--- macros/src/command/output/help/to_tokens.rs
+++ macros/src/command/output/help/to_tokens.rs
@@ -15,7 +15,6 @@ impl<'a> ToTokens for HelpOutput<'a> {
             "help" => {
                 ::std::println!("{}", #name);
                 ::std::println!("{}", #description);
-                ::std::println!();
 
                 #usage
 
